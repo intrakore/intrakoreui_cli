5bcd90711ac552c12171dab146e9b3e8e0071449
intrakore <intrakore@gmail.com>
2026-04-27 01:11:31 +0530

    feat: update boilerplates with Intrakore UI components

--- intrakoreui_cli/commands/boilerplates.py
+++ intrakoreui_cli/commands/boilerplates.py
@@ -308,29 +308,38 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import intrakoreui from 'intrakore-ui/vite';
 
-// https://vitejs.dev/config/
+// Get dynamic app and spa names from the directory structure
+const appName = path.basename(path.resolve(__dirname, '../..'));
+const spaName = path.basename(__dirname);
+
 export default defineConfig({
-	plugins: [
-		intrakoreui({
-			frappeProxy: true,
-			jinjaBootData: true,
-			lucideIcons: true,
-		}),
-		vue()
-	],
-	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, 'src')
-		}
-	},
-	build: {
-		outDir: '../{{app}}/public/{{name}}',
-		emptyOutDir: true,
-		target: 'es2015',
-	},
-	optimizeDeps: {
-		include: ['intrakore-ui > feather-icons', 'showdown', 'engine.io-client'],
-	},
+  plugins: [
+    intrakoreui({
+      frappeProxy: true,
+      jinjaBootData: true,
+      lucideIcons: true,
+      buildConfig: {
+        outDir: `../${appName}/public/${spaName}`,
+        indexHtmlPath: `../${appName}/www/${spaName}.html`,
+        emptyOutDir: true,
+        sourcemap: true,
+      },
+    }),
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  build: {
+    outDir: `../${appName}/public/${spaName}`,
+    emptyOutDir: true,
+    target: 'es2015',
+  },
+  optimizeDeps: {
+    include: ['intrakore-ui > feather-icons', 'showdown', 'engine.io-client'],
+  },
 });
 """
 
